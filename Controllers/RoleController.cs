dcbf4ae2ee42f59a968881ad8f852e4414def1a0
Albinah <albinankateko@gmail.com>
2022-02-17 08:47:09 +0200

    Add project files.

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Test_Asp_Login.Models;
+
+namespace Test_Asp_Login.Controllers
+{
+    public class RoleController : Controller
+    {
+        ApplicationDbContext context;
+        public RoleController()
+        {
+            context = new ApplicationDbContext();
+        }
+        // GET: Role
+        public ActionResult Index()
+        {
+            var Roles = context.Roles.ToList();
+            return View(Roles);
+        }
+
+        public ActionResult Create()
+        {
+            var Role = new IdentityRole();
+            return View(Role);
+        }
+
+        [HttpPost]
+        public ActionResult Create(IdentityRole Role)
+        {
+            context.Roles.Add(Role);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Test_Asp_Login.MvcApplication" Language="C#" %>
